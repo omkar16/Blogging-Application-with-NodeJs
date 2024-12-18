4b7fef6f4945fed3090efa3e9809a8b0dfd4623e
Pranav-comp <pnc9145@gmail.com>
2024-12-18 10:43:32 +0530

    Included crypto for password hashing using SHA-256

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const { Schema, Model, default: mongoose } = require("mongoose");
+const { createHmac, randomBytes } = require("crypto");
 
 const userSchema = new Schema(
   {
@@ -31,6 +32,21 @@ const userSchema = new Schema(
   },
   { timestamps: true }
 );
+
+userSchema.pre("save", function (next) {
+  const user = this;
+  if (!user.isModified("password")) return;
+
+  const salt = randomBytes(16).toString();
+  const hashedPassword = createHmac("sha256", salt)
+    .update(user.password)
+    .digest("hex");
+
+  this.salt = salt;
+  this.password = hashedPassword;
+
+  next();
+});
 
 const User = Model("user", userSchema);
 
